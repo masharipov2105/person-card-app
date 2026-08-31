1e04f15680e6b1b1719c351b95910cd64cc8068d
masharipov2105 <masharipov2105@gmail.com>
2026-08-31 20:57:29 +0500

    A CardStorageTest class was written for the CardStorage class and all methods were tested

--- src/test/java/com/masharipov2105/systems/storage/CardStorageTest.java
+++ src/test/java/com/masharipov2105/systems/storage/CardStorageTest.java
@@ -1,10 +1,52 @@
 package com.masharipov2105.systems.storage;
 
 import com.masharipov2105.systems.models.Person;
+import com.masharipov2105.systems.storage.CardStorage;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CardStorageTest{
 
-	
+	@Test
+	void testSave(){
+
+		Person p = new Person("Ali", 18, "Mangit");
+		CardStorage cd = new CardStorage();
+
+        assertEquals(null, cd.get());
+		cd.save(p);
+		assertEquals(p, cd.get());
+	}
+
+	@Test
+	void testGet(){
+
+		Person p = new Person("Ali", 18, "Mangit");
+		CardStorage cd = new CardStorage();
+		cd.save(p);
+		assertEquals(p, cd.get());
+	}
+
+	@Test
+	void testDelete(){
+
+		Person p = new Person("Ali", 18, "Mangit");
+		CardStorage cd = new CardStorage();
+		cd.save(p);
+		assertEquals(p, cd.get());
+		cd.delete();
+		assertEquals(null, cd.get());
+	}
+
+	@Test
+	void testExists(){
+
+		Person p = new Person("Ali", 18, "Mangit");
+		CardStorage cd = new CardStorage();
+
+		assertEquals(false, cd.exists());
+
+		cd.save(p);
+		assertEquals(true, cd.exists());
+	}
 }
